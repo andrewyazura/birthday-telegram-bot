a08c47c7365b9bd96c9c39694a37bdf0c59bafea
orehzzz <olegzzz749@gmail.com>
2024-08-03 15:52:41 +0300

    change conversation with /skip

--- handlers/change.py
+++ handlers/change.py
@@ -1,18 +1,21 @@
 import re
 
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     CommandHandler,
     ConversationHandler,
     MessageHandler,
     ContextTypes,
     filters,
+    CallbackQueryHandler,
 )
 from marshmallow import ValidationError
 
-from core.api_requests import put_request, get_request
+from core.api_requests import put_request, get_request, get_by_id_request
 from core.schema import BirthdaysSchema
 from handlers.fallback import stop
+
+import traceback
 
 
 CHANGE_GET_BIRTHDAY, CHANGE_NAME, CHANGE_DATE, CHANGE_NOTE = range(4)
@@ -22,27 +25,91 @@ birthdays_schema = BirthdaysSchema()
 
 
 async def change_birthday(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # request all the birthdays
-    # handle response
-    # print as a inline keyboard
+    context.user_data.clear()
+
+    try:
+        response = get_request(update.effective_user.id)
+        # handle bad response
+        # print(response.json())
+        data = response.json()
+    except Exception as e:
+        await update.message.reply_text(f"{e}. Please try again")
+        return ConversationHandler.END
+
+    keyboard = []
+    for birthday in data:
+        keyboard.append(
+            [InlineKeyboardButton(birthday["name"], callback_data=birthday["id"])]
+        )
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        "Choose whose birthday to change:", reply_markup=reply_markup
+    )
     return CHANGE_GET_BIRTHDAY
 
 
 async def change_get_birthday(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # get selected birthday, maybe add to context data
-    # ask to skip or input new name
+    # bad but okay - request selected birthday, add to context data
+    query = update.callback_query
+    await query.answer()
+
+    birthday_id = query.data
+
+    try:
+        response = get_by_id_request(update.effective_user.id, birthday_id)
+        # handle bad response
+        print(response.json())
+        birthday_json = response.json()
+    except Exception as e:
+        await update.message.reply_text(
+            f"{e}. Please try again. {traceback.format_exc()}"
+        )
+        return ConversationHandler.END
+
+    # add data to context
+    context.user_data["birthday_id"] = birthday_id
+    context.user_data["name"] = birthday_json["name"]
+    context.user_data["day"] = birthday_json["day"]
+    context.user_data["month"] = birthday_json["month"]
+    if "year" in birthday_json:
+        context.user_data["year"] = birthday_json["year"]
+    if "note" in birthday_json:
+        context.user_data["note"] = birthday_json["note"]
+
+    await query.edit_message_text(
+        text=f'Changing birthday: {birthday_json["name"]}\n\n Input a new name or send /skip to keep the same name'
+    )  # later print date and note if exist
     return CHANGE_NAME
 
 
 async def change_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # get new name
+    new_name = update.message.text
+    if len(new_name) > 255:
+        await update.message.reply_text(
+            "That name is too long. Please choose a shorter one or send /skip to keep the same name:"
+        )
+        print("returning ADD_NAME")
+        return CHANGE_NAME
+    if new_name == context.user_data["name"]:
+        await update.message.reply_text(
+            "This name is the same. Input a new name or send /skip to keep the same name:"
+        )
+        return CHANGE_NAME
+    context.user_data["new_name"] = new_name
+
+    await update.message.reply_text(
+        "Great! Enter the date (format: `DD.MM.YYYY` or `DD.MM`) or send /skip to keep the same date:"
+    )
     # ask user for new date or skip
     return CHANGE_DATE
 
 
 async def skip_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # name sould remain the same!!!
-    # ask user for new date or skip
+    await update.message.reply_text(
+        "Enter the date (format: `DD.MM.YYYY` or `DD.MM`) or send /skip to keep the same date:"
+    )
     return CHANGE_DATE
 
 
@@ -80,11 +147,9 @@ async def put_birthday(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 change_conv_handler = ConversationHandler(
-    entry_points=[CommandHandler("change_birtday", change_birthday)],
+    entry_points=[CommandHandler("change_birthday", change_birthday)],
     states={
-        CHANGE_GET_BIRTHDAY: [
-            MessageHandler(filters.TEXT & ~filters.COMMAND, change_get_birthday)
-        ],
+        CHANGE_GET_BIRTHDAY: [CallbackQueryHandler(change_get_birthday, r"^[1-9]\d*$")],
         CHANGE_NAME: [
             MessageHandler(filters.TEXT & ~filters.COMMAND, change_name),
             CommandHandler("skip", skip_name),
